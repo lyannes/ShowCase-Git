df290c15bccfbddb53bb8f5c4d9044260555ae33
lyannes <116569318+lyannes@users.noreply.github.com>
2024-02-27 08:39:32 +0100

    alert message aanpassen

--- js/mail.js
+++ js/mail.js
@@ -1,29 +1,3 @@
-// function sendEmail() {
-//   var form = document.getElementById("contactform");
-//   var formData = new FormData(form);
-
-//   fetch("http://localhost:5068/Mail/sendEmail", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(Object.fromEntries(formData)),
-//   })
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-
-//       return response.text();
-//     })
-//     .then((data) => {
-//       console.log("Success:", data);
-//       alert("The mail is on the plane way to the northpole!");
-//       resetForm();
-//     })
-//     .catch((error) => {
-//       alert("The mail did not make it on the plane way to the northpole.");
-//       console.error("Error:", error);
-//     });
-// }
 function sendEmail() {
   var sendButton = document.getElementById("sendButton");
   var spinner = document.getElementById("spinner");
@@ -48,7 +22,7 @@ function sendEmail() {
     })
     .then((data) => {
       console.log("Success:", data);
-      alert("The mail is on the plane way to the northpole!");
+      alert("The mail is on a plane to the northpole!");
       resetForm();
     })
     .catch((error) => {
